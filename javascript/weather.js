0304d0387e8c251cf3a23319298fbd4794977a3b
Jarnin Fang <jarninfang@gmail.com>
2016-02-06 04:15:06 -0800

    pls

--- javascript/weather.js
+++ javascript/weather.js
@@ -24,7 +24,7 @@ function showPosition(position) {
 		type: "GET",
 		success: function(xml) {
 			console.log("success");
-			console.log(xml.name);
+			console.log(xml.getElementById(name));
 		},
 		error: function( xhr, status, errorThrown ) {
 			alert( "Sorry, there was a problem!" );
