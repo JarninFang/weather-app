65ba7dadc5b960d3b29e3c9d5556e238ff81756d
Jarnin Fang <jarninfang@gmail.com>
2016-02-06 04:01:17 -0800

    test

--- javascript/weather.js
+++ javascript/weather.js
@@ -6,8 +6,39 @@ $(document).ready(function() {
 });
 
 function showPosition(position) {
-	console.log("Success");
+	var getUrl = "http://api.geonames.org/findNearbyPlaceName?";
+	//Convert latitude and longitude into URL params
+	var coordParams = {
+		lat: position.coords.latitude,
+		lng: position.coords.longitude,
+		username: "demo"
+	};
+
+	var str = jQuery.param(coordParams);
+
+	getUrl = getUrl + str;
+	console.log("The url is: " + getUrl);
+	//Make ajax call to API to get city name
+	$.ajax({
+		url: getUrl,
+		type: "GET",
+		success: function(xml) {
+			console.log("success");
+			console.log("Here is the xml" + xml);
+		},
+		error: function( xhr, status, errorThrown ) {
+			alert( "Sorry, there was a problem!" );
+			console.log( "Error: " + errorThrown );
+			console.log( "Status: " + status );
+			console.dir( xhr );
+		},
+		complete: function( xhr, status ) {
+			alert( "The request is complete!" );
+		}	
+	});
+	$('#location-text').text();
 }
+
 function fail(err) {
 	console.warn(err.message);
 }
